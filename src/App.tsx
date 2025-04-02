91311ced7e02174efba098f22b60e24703b7b2fe
hdhrhb <k5a9ev8r@yahoo.co.jp>
2025-04-02 14:46:04 +0900

    import Amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Amplify } from "aws-amplify";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("Amplify.configure:");
+    console.log(Amplify.getConfig());
+  }, []);
 
   return (
     <>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
